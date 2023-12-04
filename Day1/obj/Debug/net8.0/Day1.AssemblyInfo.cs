986e3abda4a829363993ea1346a8667a57f613c0
Octanoz <130052848+Octanoz@users.noreply.github.com>
2023-12-04 15:20:32 +0100

    Solved Day4

--- Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
+++ Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a67f4cd1e2284396bb994dbaee2c5896af41f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfece1383006ec0d3256991eeef5bbd5a572c61f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Day4/Program.cs
@@ -0,0 +1,54 @@
+﻿// string filePath = @"..\Day4\example1.txt";
+string filePath = @"..\Day4\input.txt";
+ReadOnlySpan<string> input = File.ReadAllLines(filePath);
+
+int resultOne = PartOne(input);
+Console.WriteLine($"Total points in part one: {resultOne}");
+
+int resultTwo = PartTwo(input);
+Console.WriteLine($"Total cards in part one: {resultTwo}");
+
+int PartOne(ReadOnlySpan<string> input)
+{
+    int points = 0;
+    foreach (var line in input)
+    {
+        string[] parts = line.Split([':', '|']);
+        int[] winners = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int[] givenNumbers = parts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        int[] winningNumbers = winners.Intersect(givenNumbers).ToArray();
+
+        points += winningNumbers.Length == 0 ? 0 : 1 << winningNumbers.Length - 1;
+    }
+
+    return points;
+}
+
+int PartTwo(ReadOnlySpan<string> input)
+{
+    int[] totalCards = new int[input.Length];
+    int index = 0;
+    while (index < input.Length)
+    {
+        totalCards[index]++;
+        string[] parts = input[index].Split([':', '|']);
+        int[] winners = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int[] givenNumbers = parts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        int cardPoints = winners.Intersect(givenNumbers).Count();
+        int copies = totalCards[index];
+
+        for (int i = 1; i <= cardPoints; i++)
+        {
+            totalCards[index + i] += copies;
+        }
+
+        index++;
+    }
+
+    return totalCards.Sum();
+}
+
+
+
