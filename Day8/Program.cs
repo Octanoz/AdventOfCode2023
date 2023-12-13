72eb097a3ad1342cacb9af3147e4fc156b8a66b6
Octanoz <130052848+Octanoz@users.noreply.github.com>
2023-12-13 08:42:55 +0100

    Solved Day9

---
+++ Day8/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Text.RegularExpressions;
+
+// string filePath = @"..\Day8\example1.txt";
+// string filePath = @"..\Day8\example2.txt";
+// string filePath = @"..\Day8\example3.txt";
+string filePath = @"..\Day8\input.txt";
+ReadOnlySpan<string> input = File.ReadAllLines(filePath);
+
+// int resultOne = PartOne(input);
+// Console.WriteLine($"It took {resultOne} steps to get to 'ZZZ' in part one.");
+
+ulong resultTwo = PartTwo(input, []);
+Console.WriteLine($"It took {resultTwo} steps to travel from all 'xxA' nodes to all 'xxZ' nodes in part two.");
+
+//? 105537351152520 was too high
+
+int PartOne(ReadOnlySpan<string> input)
+{
+    List<MapNode> nodes = [];
+    string pattern = @"(\w+)";
+    string directions = input[0];
+
+    int len = input.Length;
+    for (int i = 2; i < len; i++)
+    {
+        var parts = Regex.Matches(input[i], pattern)
+                                                .Cast<Match>()
+                                                .Select(m => m.Value)
+                                                .ToArray();
+
+        MapNode newNode = new(parts[0], parts[1] == parts[0] ? null : parts[1], parts[2] == parts[0] ? null : parts[2]);
+        nodes.Add(newNode);
+    }
+
+    MapNode currentNode = nodes.First(n => n.Name == "AAA");
+    int steps = 0;
+    int dirLen = directions.Length;
+    while (currentNode.Name != "ZZZ")
+    {
+        int index = steps % dirLen;
+        char direction = directions[index];
+
+        if (direction == 'L')
+            currentNode = nodes.First(n => n.Name == currentNode.Left);
+        else currentNode = nodes.First(n => n.Name == currentNode.Right);
+
+        steps++;
+    }
+
+    return steps;
+}
+
+ulong PartTwo(ReadOnlySpan<string> input, List<MapNode> travellingNodes)
+{
+    List<MapNode> nodes = [];
+    string pattern = @"(\w+)";
+    string directions = input[0];
+
+    int len = input.Length;
+    for (int i = 2; i < len; i++)
+    {
+        var parts = Regex.Matches(input[i], pattern)
+                                                .Cast<Match>()
+                                                .Select(m => m.Value)
+                                                .ToArray();
+
+        MapNode newNode = new(parts[0], parts[1] == parts[0] ? null : parts[1], parts[2] == parts[0] ? null : parts[2]);
+        nodes.Add(newNode);
+
+        if (parts[0].EndsWith('A'))
+            travellingNodes.Add(newNode);
+    }
+
+    int nodesCount = travellingNodes.Count;
+    ulong[] stepsTaken = new ulong[nodesCount];
+
+    int dirLen = directions.Length;
+    for (int i = 0; i < nodesCount; i++)
+    {
+        int steps = 0;
+        while (!travellingNodes[i].Name.EndsWith('Z'))
+        {
+            int index = steps % dirLen;
+            char direction = directions[index];
+            if (direction == 'L')
+                travellingNodes[i] = nodes.First(n => n.Name == travellingNodes[i].Left);
+            else travellingNodes[i] = nodes.First(n => n.Name == travellingNodes[i].Right);
+
+            steps++;
+        }
+
+        stepsTaken[i] = (ulong)steps;
+    }
+
+    ulong lcm = stepsTaken[0];
+    for (int i = 1; i < stepsTaken.Length; i++)
+    {
+        lcm = LowestCommonMultiple(lcm, stepsTaken[i]);
+    }
+
+    return lcm;
+}
+
+ulong LowestCommonMultiple(ulong a, ulong b) => a * b / GreatestCommonDivisor(a, b);
+
+ulong GreatestCommonDivisor(ulong a, ulong b)
+{
+    while (b != 0)
+    {
+        ulong temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
+
+record MapNode(string Name, string? Left, string? Right);
---
+++ Day9/Program.cs
@@ -0,0 +1,83 @@
+﻿// string filePath = @"..\Day9\example1.txt";
+string filePath = @"..\Day9\input.txt";
+ReadOnlySpan<string> input = File.ReadAllLines(filePath);
+
+int resultOne = PartOne(input);
+Console.WriteLine($"Total histories in part one: {resultOne}");
+
+int resultTwo = PartTwo(input);
+Console.WriteLine($"Total histories in part two: {resultTwo}");
+
+int PartOne(ReadOnlySpan<string> input)
+{
+    int total = 0;
+    foreach (var line in input)
+    {
+        List<List<int>> histories = [];
+        List<int> origin = line.Split(' ').Select(int.Parse).ToList();
+        histories.Add(origin);
+
+        for (int i = 0; i < histories.Count; i++)
+        {
+            List<int> currentList = histories[i];
+            int len = currentList.Count;
+            List<int> newList = [];
+
+            for (int j = 0; j < len - 1; j++)
+            {
+                newList.Add(currentList[j + 1] - currentList[j]);
+            }
+
+            if (newList.All(i => i == 0))
+                break;
+
+            histories.Add(newList);
+        }
+
+        for (int i = histories.Count - 1; i >= 1; i--)
+        {
+            histories[i - 1].Add(histories[i][^1] + histories[i - 1][^1]);
+        }
+
+        total += histories[0][^1];
+    }
+
+    return total;
+}
+
+int PartTwo(ReadOnlySpan<string> input)
+{
+    int total = 0;
+    foreach (var line in input)
+    {
+        List<List<int>> histories = [];
+        List<int> origin = line.Split(' ').Select(int.Parse).ToList();
+        histories.Add(origin);
+
+        for (int i = 0; i < histories.Count; i++)
+        {
+            List<int> currentList = histories[i];
+            int len = currentList.Count;
+            List<int> newList = [];
+
+            for (int j = 0; j < len - 1; j++)
+            {
+                newList.Add(currentList[j + 1] - currentList[j]);
+            }
+
+            if (newList.All(i => i == 0))
+                break;
+
+            histories.Add(newList);
+        }
+
+        for (int i = histories.Count - 1; i >= 1; i--)
+        {
+            histories[i - 1].Insert(0, histories[i - 1][0] - histories[i][0]);
+        }
+
+        total += histories[0][0];
+    }
+
+    return total;
+}
